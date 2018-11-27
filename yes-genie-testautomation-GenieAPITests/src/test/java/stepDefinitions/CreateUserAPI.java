9423d4672e8554c245a55f11e236639efa9ddaa7
Vibhu Gupta <gupta.vibhu1@gmail.com>
2018-11-27 14:43:07 +0530

    test case created for create user, search user

--- yes-genie-testautomation-GenieAPITests/src/test/java/stepDefinitions/CreateUserAPI.java
+++ yes-genie-testautomation-GenieAPITests/src/test/java/stepDefinitions/CreateUserAPI.java
@@ -1,12 +1,14 @@
 package stepDefinitions;
 
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
-import io.restassured.http.ContentType;
-import io.restassured.response.Response;
-import static net.serenitybdd.rest.SerenityRest.given;
-import static utils.Utilities.matchesJsonSchema;
+        import cucumber.api.java.en.Given;
+        import cucumber.api.java.en.Then;
+        import cucumber.api.java.en.When;
+        import io.restassured.http.ContentType;
+        import io.restassured.response.Response;
+        import org.apache.http.HttpStatus;
+        import static net.serenitybdd.rest.SerenityRest.given;
+        import static org.hamcrest.Matchers.is;
+        import static utils.Utilities.matchesJsonSchema;
 
 /**
  * Created by vibhu on 11/26/2018.
@@ -14,7 +16,7 @@ import static utils.Utilities.matchesJsonSchema;
 public class CreateUserAPI {
 
     public static Response response;
-    public static Response responseUpdatedRoleContractBaseApi;
+    public static Response responseUpdatedRoleContractBaseApi, responseSearchUserContractBaseApi, responseCreateUserContractBaseApi, responseGetCreatedUserContractBaseApi, responseModifyRoleContractBaseApi, responseDeleteContractBaseApi;
     public static String uri;
 
     @When("^a genie user creates a user$")
@@ -36,10 +38,105 @@ public class CreateUserAPI {
                 body(matchesJsonSchema(fileName));
     }
 
-    @When ("^a genie user searches the role on user management tab$")
-    public void getResponseFromUpdatedRoleContractBaseApi(){
+    @When("^a genie user searches the role on user management tab$")
+    public void getResponseFromUpdatedRoleContractBaseApi() {
         responseUpdatedRoleContractBaseApi = given().accept(ContentType.JSON).
-                  get(uri);
+                get(uri);
     }
 
+    @Then("^a genie user get the status code 200 as a response from the user management api$")
+    public void validateAPIisOK() {
+        responseUpdatedRoleContractBaseApi.
+                then().assertThat().statusCode(HttpStatus.SC_OK);
+    }
+
+    @Then("^a genie user get the response from the user management api for the initial record for \"([^\"]*)\" is \"([^\"]*)\"$")
+    public void respose_for_key_and_value_is(String key, String value) throws Throwable {
+        responseUpdatedRoleContractBaseApi.then().
+                body(("records[0].".concat(key)), is(value));
+    }
+
+    @When("^a genie user searches the user with the \"([^\"]*)\"$")
+    public void getResponseFromSearchUserContractBaseApi(String userId) {
+        responseSearchUserContractBaseApi = given().accept(ContentType.JSON).
+                param("userId", userId).
+                post(uri);
+    }
+
+    @Then("^a genie user get the status code 200 as a response from the search user api$")
+    public void validateSearchUserAPIisOK() {
+        responseSearchUserContractBaseApi.
+                then().assertThat().statusCode(HttpStatus.SC_OK);
+    }
+
+    @Then("^a genie user get the response from the search user api for the initial record for \"([^\"]*)\" is \"([^\"]*)\"$")
+    public void respose_for_search_user_api_key_and_value_is(String key, String value) throws Throwable {
+        responseSearchUserContractBaseApi.then().
+                body(("records[0].".concat(key)), is(value));
+    }
+
+    @Then("^a genie user get the status code 103 as a response from the search user api as user does not exist$")
+    public void validateSearchUserHavingStatusCodeOneZeroThree() {
+        responseSearchUserContractBaseApi.
+                then().assertThat().statusCode(103);
+    }
+
+    @Then("^a genie user get the status code 104 as a response from the search user api as user already exist$")
+    public void validateSearchUserHavingStatusCodeOneZeroFour() {
+        responseSearchUserContractBaseApi.
+                then().assertThat().statusCode(104);
+    }
+
+    @When("^a genie user creates the user with the \"([^\"]*)\"$")
+    public void getResponseFromCreateUserContractBaseApi(String userId) {
+        responseCreateUserContractBaseApi = given().accept(ContentType.JSON).
+                param("userId", userId).
+                post(uri);
+    }
+
+    @Then("^a genie user get the status code 200 as a response from the create user api$")
+    public void validateCreateUserAPIisOK() {
+        responseCreateUserContractBaseApi.
+                then().assertThat().statusCode(HttpStatus.SC_OK);
+    }
+
+    @When("^a genie user gets the user on view users page with the \"([^\"]*)\"$")
+    public void getResponseFromgetCreatedUserContractBaseApi(String userId) {
+        responseGetCreatedUserContractBaseApi = given().accept(ContentType.JSON).
+                param("userId", userId).
+                post(uri);
+    }
+
+    @Then("^a genie user get the status code 200 as a response from the view users api$")
+    public void validateGetCreatedUserAPIisOK() {
+        responseCreateUserContractBaseApi.
+                then().assertThat().statusCode(HttpStatus.SC_OK);
+    }
+
+    @When("^a genie user modify the \"([^\"]*)\" on view users page for the \"([^\"]*)\"$")
+    public void getResponseFromModifyRoleContractBaseApi(String role, String userId) {
+        responseModifyRoleContractBaseApi = given().accept(ContentType.JSON).
+                param("role", role).
+                param("userId", userId).
+                post(uri);
+    }
+
+    @Then("^a genie user get the status code 200 as a response from the modify role api$")
+    public void validateModifyRoleAPIisOK() {
+        responseModifyRoleContractBaseApi.
+                then().assertThat().statusCode(HttpStatus.SC_OK);
+    }
+
+    @When("^a genie user delete the user on view users page for the \"([^\"]*)\"$")
+    public void getResponseFromDeleteContractBaseApi(String userId) {
+        responseDeleteContractBaseApi = given().accept(ContentType.JSON).
+                param("userId", userId).
+                post(uri);
+    }
+
+    @Then("^a genie user get the status code 200 as a response from the delete role api$")
+    public void validateDeleteAPIisOK() {
+        responseDeleteContractBaseApi.
+                then().assertThat().statusCode(HttpStatus.SC_OK);
+    }
 }
--- yes-genie-testautomation-GenieAPITests/src/test/resources/apiFeatures/createUser.feature
+++ yes-genie-testautomation-GenieAPITests/src/test/resources/apiFeatures/createUser.feature
@@ -1,4 +1,4 @@
-@search
+@usermanagement
 Feature: Validate Create User API
 
   Background: User has api to test
@@ -14,7 +14,78 @@ Feature: Validate Create User API
       | updatedRoleSchema.json               |
       | createUser.json                      |
 
-    Scenario: user validates the response from the API
-      When a genie user searches the role on user management tab
-      Then a user get the status code 200 as a response from the api
+  @smoke @regression
+  Scenario: user validates the response code from the API
+    When a genie user searches the role on user management tab
+    Then a genie user get the status code 200 as a response from the user management api
 
+  @regression
+  Scenario Outline: user validates the response from the API
+    When a genie user searches the role on user management tab
+    Then a genie user get the response from the user management api for the initial record for "<key>" is "<value>"
+    Examples:
+      | key          | value    |
+      | customerName | Foo6 foo |
+      | city         | goa      |
+
+  @smoke @regression
+  Scenario Outline: user validates the response code from the search user API
+    When a genie user searches the user with the "<userId>"
+    Then a genie user get the status code 200 as a response from the search user api
+    Examples:
+      | userId |
+      | userId |
+
+  @regression
+  Scenario Outline: user validates the response body from the search user API
+    When a genie user searches the user with the "userId"
+    Then a genie user get the response from the search user api for the initial record for "<key>" is "<value>"
+    Examples:
+      | key    | value    |
+      | userId | Foo6 foo |
+      | name   | Foo6 foo |
+      | email  | Foo6 foo |
+      | mobile | Foo6 foo |
+
+  @regression
+  Scenario: user validates the response from the search user API for the case where user does not exist
+    When a genie user searches the user with the "userId"
+    Then a genie user get the status code 103 as a response from the search user api as user does not exist
+
+
+  @regression
+  Scenario: user validates the response from the search user API for the case where user already exist
+    When a genie user searches the user with the "userId"
+    Then a genie user get the status code 104 as a response from the search user api as user already exist
+
+  @smoke @regression
+  Scenario Outline: user validates the response code from the create user API
+    When a genie user creates the user with the "<userId>"
+    Then a genie user get the status code 200 as a response from the create user api
+    Examples:
+      | userId |
+      | userId |
+
+  @smoke @regression
+  Scenario Outline: user validates the response code from the get created user API
+    When a genie user gets the user on view users page with the "<userId>"
+    Then a genie user get the status code 200 as a response from the view users api
+    Examples:
+      | userId |
+      | userId |
+
+  @regression
+  Scenario Outline: user validates the response code from the edit role API
+    When a genie user modify the "<role>" on view users page for the "<userId>"
+    Then a genie user get the status code 200 as a response from the modify role api
+    Examples:
+      | role | userId |
+      | role | userId |
+
+  @regression
+  Scenario Outline: user validates the response code from the edit role API
+    When a genie user delete the user on view users page for the "<userId>"
+    Then a genie user get the status code 200 as a response from the delete role api
+    Examples:
+      | userId |
+      | userId |
