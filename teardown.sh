66760bb370132d0b0eae120068895556275c8875
Prabhat <prabhat.arya@xebia.com>
2019-01-07 11:45:40 +0530

    making changes related to gitignore

--- teardown.sh
+++ teardown.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 /bin/bash ./xebia_elasticdata/src/test/script/hbase_data_deletion.sh
-curl -XDELETE 'http://localhost:9200/yesbank/'
+curl -XDELETE 'http://localhost:9200/gny_cust_search/'
 echo -e "Teardown executed"
