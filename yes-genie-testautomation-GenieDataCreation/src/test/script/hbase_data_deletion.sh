5153c3cf153a09fc7c2138c9d939be945f38e4aa
prabhat-xebia <45930570+prabhat-xebia@users.noreply.github.com>
2019-01-08 17:50:46 +0530

    Update hbase_data_deletion.sh

--- yes-genie-testautomation-GenieDataCreation/src/test/script/hbase_data_deletion.sh
+++ yes-genie-testautomation-GenieDataCreation/src/test/script/hbase_data_deletion.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 LOG_FILE="./log/hbaseDataDeletion.log"
-curl -XDELETE 'http://localhost:9200/gny_cust_search/'
+curl -XDELETE 'http://192.168.7.51:9200/gny_cust_search/'
 sshpass -p "cloudera" ssh -o StrictHostKeyChecking=no  -p 3022 cloudera@192.168.3.9 << 'EOF'
 echo -e "truncate 'genie_cust_demog'"| hbase shell
 echo -e "truncate 'genie_recomendations'"| hbase shell
