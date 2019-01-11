4b60b1f7d1ff8e1d02a88aad47f2a949f291c963
Prabhat <prabhat.arya@xebia.com>
2019-01-11 16:30:38 +0530

    set up script changes for xls format

--- task_script/setup.sh
+++ task_script/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#cd yes-genie-testautomation-GenieDataCreation
+cd yes-genie-testautomation-GenieDataCreation
 pwd
-#gradle clean test
-#echo -e "xebia_elasticdata completed successfully"
-#cd ..
+gradle clean test
+echo -e "xebia_elasticdata completed successfully"
+cd ..
 /bin/bash ./yes-genie-testautomation-GenieDataCreation/src/test/script/hbase_genie_cust_demog.sh ./yes-genie-testautomation-GenieDataCreation/src/main/resources/demogs.xlsx
 echo -e "demogs executed"
 /bin/bash ./yes-genie-testautomation-GenieDataCreation/src/test/script/hbase_genie_recomendations.sh ./yes-genie-testautomation-GenieDataCreation/src/main/resources/NBA.xlsx
