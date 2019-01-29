fb889d0fe0ca62d95d0caf4070b849de56783871
Prabhat <prabhat.arya@xebia.com>
2019-01-29 11:04:05 +0530

    configuration for client in ui

--- task_script/setup.sh
+++ task_script/setup.sh
@@ -10,14 +10,4 @@ pwd
 gradle clean test
 echo -e "xebia_elasticdata and hbasedata uploaded successfully"
 
-#cd yes-genie-testautomation-GenieAPITests
-#gradle clean test
-#echo -e "yes-genie-testautomation-GenieAPITests completed successfully"
-#cd ../yes-genie-testautomation-GenieUITests
-#gradle clean test
-#echo -e "yes-genie-testautomation-GenieUITests completed successfully"
-#/bin/bash ./xebia_elasticdata/src/test/script/hbase_data_deletion.sh
-#curl -XDELETE 'http://localhost:9200/yesbank/'
-#echo -e "Teardown executed"
-
 
