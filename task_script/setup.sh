c1c9b00f434d18287de9bed7aa26a615bdd62d8b
adalakoti <37995706+adalakoti@users.noreply.github.com>
2019-02-06 17:52:38 +0530

    Update setup.sh

--- task_script/setup.sh
+++ task_script/setup.sh
@@ -5,9 +5,9 @@
 echo -e "demogs executed"
 /bin/bash ./yes-genie-testautomation-GenieDataCreation/src/test/script/hbase_genie_recomendations.sh ./yes-genie-testautomation-GenieDataCreation/src/main/resources/NBA.xlsx
 echo -e "NBA executed"
-# cd yes-genie-testautomation-GenieDataCreation
-# pwd
-# gradle clean test
+cd yes-genie-testautomation-GenieDataCreation
+pwd
+gradle clean test
 echo -e "xebia_elasticdata and hbasedata uploaded successfully"
 
 
